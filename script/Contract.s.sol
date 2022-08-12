c36816a377e77be5303d5d3b5a6aa462ba10966a
Wilson Cusack <cusackw@gmail.com>
2022-08-11 21:19:30 -0400

    added to script

--- script/Contract.s.sol
+++ script/Contract.s.sol
@@ -2,14 +2,36 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
+import {ERC20} from "solmate/tokens/ERC20.sol";
+import {ERC721} from "solmate/tokens/ERC721.sol";
+
 import {DebtToken} from "src/DebtToken.sol";
 import {Oracle} from "src/squeeth/Oracle.sol";
+import {StrategyFactory} from "src/StrategyFactory.sol";
+
+contract TestERC20 is ERC20("USDC", "USDC", 18) {
+    function mint(address to, uint256 amount) external {
+        _mint(to, amount);
+    }
+}
+
+contract TestERC721 is ERC721("APE", "APE") {
+    function mint(address to, uint256 id) external {
+        _mint(to, id);
+    }
+
+    function tokenURI(uint256 id) public view override returns (string memory) {
+
+    }
+}
 
 contract ContractScript is Script {
     function setUp() public {}
 
     function run() public {
         vm.startBroadcast();
-        new Oracle();
+        new TestERC20();
+        new TestERC721();
+        new StrategyFactory();
     }
 }
