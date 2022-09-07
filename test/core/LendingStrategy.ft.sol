ed3e5c63d5a0ea3b6f374692a3bfd8f7dcf7df2d
Wilson Cusack <cusackw@gmail.com>
2022-09-07 12:36:03 -0400

    tested

--- test/core/LendingStrategy.ft.sol
+++ test/core/LendingStrategy.ft.sol
@@ -68,6 +68,24 @@ contract LendingStrategyForkingTest is Test, MainnetForking {
         _populateOnReceivedArgs();
     }
 
+    function testAddCollateral() public {
+        (uint256 vaultId, ) = strategy.openVault(borrower);
+        vm.startPrank(borrower);
+        nft.approve(address(strategy), collateralId);
+        strategy.addCollateral(vaultId, ILendingStrategy.Collateral(nft, collateralId), oracleInfo, sig);
+    }
+
+    function testAddCollateralMulticall() public {
+        (uint256 vaultId, ) = strategy.openVault(borrower);
+        nft.mint(borrower, collateralId + 1);
+        vm.startPrank(borrower);
+        nft.setApprovalForAll(address(strategy), true);
+        bytes[] memory data = new bytes[](2);
+        data[0] = abi.encodeWithSelector(strategy.addCollateral.selector, vaultId, ILendingStrategy.Collateral(nft, collateralId), oracleInfo, sig);
+        data[1] = abi.encodeWithSelector(strategy.addCollateral.selector, vaultId, ILendingStrategy.Collateral(nft, collateralId + 1), oracleInfo, sig);
+        strategy.multicall(data);
+    }
+
     function testOpenVaultAddDebtAndSwap() public {
         vm.startPrank(borrower);
         safeTransferReceivedArgs.minOut = 1;
