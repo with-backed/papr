87034bbc678c1135981d360c1ae795cd92a9d18b
Adam Gobran <adamgobes@gmail.com>
2022-10-07 10:42:54 -0400

    more cleanup

--- test/OracleSigUtils.sol
+++ test/OracleSigUtils.sol
@@ -1,16 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import { ILendingStrategy } from "src/interfaces/ILendingStrategy.sol";
+import {ILendingStrategy} from "src/interfaces/ILendingStrategy.sol";
 
 contract OracleSigUtils {
     bytes32 internal DOMAIN_SEPARATOR;
 
-    // keccak256("Permit(address owner,address spender,uint256 value,uint256 nonce,uint256 deadline)");
     bytes32 public constant MESSAGE_TYPEHASH =
         keccak256("Message(bytes32 id,bytes payload,uint256 timestamp)");
 
-    // computes the hash of a permit
     function getStructHash(ILendingStrategy.OracleMessage memory message)
         internal
         pure
@@ -27,7 +25,6 @@ contract OracleSigUtils {
             );
     }
 
-    // computes the hash of the fully encoded EIP-712 message for the domain, which can be used to recover the signer
     function getTypedDataHash(ILendingStrategy.OracleMessage memory message)
         public
         view
