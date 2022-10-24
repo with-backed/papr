8b64c93821f831d377a5f6580ee53e9b6a8b46e5
Adam Gobran <adamgobes@gmail.com>
2022-10-24 15:18:33 -0400

    use OZ

--- src/pvp/PVPUSDC.sol
+++ src/pvp/PVPUSDC.sol
@@ -1,11 +1,11 @@
 pragma solidity ^0.8.13;
 
-import {ERC20} from "solmate/tokens/ERC20.sol";
+import {ERC20} from "openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import {BoringOwnable} from "@boringsolidity/BoringOwnable.sol";
 import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
 import {SafeCast} from "v3-core/contracts/libraries/SafeCast.sol";
 
-contract PVPUSDC is ERC20("Backed PVP USDC", "PVPUSDC", 6), BoringOwnable {
+contract PVPUSDC is ERC20("Backed PVP USDC", "pUSDC"), BoringOwnable {
     using SafeCast for uint256;
 
     struct Stake {
@@ -13,7 +13,9 @@ contract PVPUSDC is ERC20("Backed PVP USDC", "PVPUSDC", 6), BoringOwnable {
         uint256 depositedAt;
     }
 
-    mapping(address => Stake) public balance;
+    mapping(address => Stake) public stakedBalance;
+
+    error StakingTooMuch();
 
     uint256 public constant SECONDS_PER_YEAR = 365 days;
     uint256 public APR = 1.1e18;
@@ -22,27 +24,44 @@ contract PVPUSDC is ERC20("Backed PVP USDC", "PVPUSDC", 6), BoringOwnable {
         transferOwnership(msg.sender, false, false);
     }
 
+    function decimals() public view override returns (uint8) {
+        return 6;
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        return super.balanceOf(account) - stakedBalance[account].amount;
+    }
+
     function mint(address to, uint256 amount) external onlyOwner {
         _mint(to, amount);
     }
 
+    function burn(uint256 amount) external {
+        _burn(msg.sender, amount);
+    }
+
     function stake(uint256 amountToStake) public {
-        Stake memory currentStake = balance[msg.sender];
+        if (amountToStake > balanceOf(msg.sender)) {
+            revert StakingTooMuch();
+        }
+
+        Stake memory currentStake = stakedBalance[msg.sender];
         uint256 newAmount = amountToStake;
         if (currentStake.amount != 0) {
-            newAmount = _calculateNewBalanceFromStake(currentStake);
+            newAmount =
+                _calculateNewBalanceFromStake(currentStake) +
+                amountToStake;
         }
         Stake memory newStake = Stake({
             amount: newAmount,
             depositedAt: block.timestamp
         });
-        balance[msg.sender] = newStake;
-        _burn(msg.sender, amountToStake);
+        stakedBalance[msg.sender] = newStake;
     }
 
     function unstake() public returns (uint256 total) {
-        Stake memory stake = balance[msg.sender];
-        delete balance[msg.sender];
+        Stake memory stake = stakedBalance[msg.sender];
+        delete stakedBalance[msg.sender];
 
         uint256 secondsElapsed = block.timestamp - stake.depositedAt;
         uint256 ratio = FixedPointMathLib.divWadDown(
@@ -51,11 +70,22 @@ contract PVPUSDC is ERC20("Backed PVP USDC", "PVPUSDC", 6), BoringOwnable {
         );
 
         total = _calculateNewBalanceFromStake(stake);
-        _mint(msg.sender, total);
+        _mint(msg.sender, total - stake.amount);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override {
+        if (amount > balanceOf(from) && from != address(0)) {
+            revert("ERC20: transfer amount exceeds balance");
+        }
     }
 
     function _calculateNewBalanceFromStake(Stake memory stake)
         public
+        view
         returns (uint256 newBalance)
     {
         uint256 secondsElapsed = block.timestamp - stake.depositedAt;
--- test/pvp/PVPUSDC.t.sol
+++ test/pvp/PVPUSDC.t.sol
@@ -25,11 +25,25 @@ contract PVPUSDCTest is Test {
         assertEq(amount, pvpUSDC.balanceOf(address(1)));
     }
 
+    function testNameSymbolDecimals() public {
+        assertEq(pvpUSDC.symbol(), "pUSDC");
+        assertEq(pvpUSDC.name(), "Backed PVP USDC");
+        assertEq(pvpUSDC.decimals(), 6);
+    }
+
+    function testBurnWorks() public {
+        pvpUSDC.mint(address(1), amount);
+        vm.prank(address(1));
+        pvpUSDC.burn(amount);
+        assertEq(pvpUSDC.balanceOf(address(1)), 0);
+    }
+
     function testStakingWorks() public {
         pvpUSDC.mint(address(1), amount);
         vm.startPrank(address(1));
-        pvpUSDC.approve(address(1), type(uint256).max);
+
         pvpUSDC.stake(amount);
+        assertEq(pvpUSDC.balanceOf(address(1)), 0);
 
         vm.warp(block.timestamp + 365 days);
         uint256 total = pvpUSDC.unstake();
@@ -39,26 +53,49 @@ contract PVPUSDCTest is Test {
         vm.stopPrank();
     }
 
+    function testStakingFailsIfStakingMoreThanBalance() public {
+        pvpUSDC.mint(address(1), amount);
+        vm.startPrank(address(1));
+
+        pvpUSDC.stake(amount);
+        vm.expectRevert(PVPUSDC.StakingTooMuch.selector);
+        pvpUSDC.stake(amount);
+
+        vm.stopPrank();
+    }
+
+    function testBeforeTokenTransferWorks() public {
+        pvpUSDC.mint(address(1), amount);
+        vm.startPrank(address(1));
+
+        pvpUSDC.stake(amount);
+        vm.expectRevert("ERC20: transfer amount exceeds balance");
+        pvpUSDC.transfer(address(2), 100);
+        vm.stopPrank();
+    }
+
     function testStakingWorksWhenStakingTwice() public {
         pvpUSDC.mint(address(1), amount);
         vm.startPrank(address(1));
-        pvpUSDC.approve(address(1), type(uint256).max);
+
         pvpUSDC.stake(amount / 2);
         assertEq(pvpUSDC.balanceOf(address(1)), amount / 2);
 
         uint256 newTimestamp = block.timestamp + 365 days;
-
         vm.warp(newTimestamp);
+
         pvpUSDC.stake(amount / 2);
 
-        (uint256 amount, uint256 depositedAt) = pvpUSDC.balance(address(1));
-        assertEq(amount, 54);
+        (uint256 amount, uint256 depositedAt) = pvpUSDC.stakedBalance(
+            address(1)
+        );
+        assertEq(amount, 104);
         assertEq(depositedAt, newTimestamp);
 
         vm.warp(newTimestamp + 365 days);
-        pvpUSDC.unstake();
 
-        assertEq(pvpUSDC.balanceOf(address(1)), 59);
+        pvpUSDC.unstake();
+        assertEq(pvpUSDC.balanceOf(address(1)), 110);
 
         vm.stopPrank();
     }
