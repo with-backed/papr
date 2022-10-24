e4b9bc5c4920f88675d454d6f51f7a29b8293829
Adam Gobran <adamgobes@gmail.com>
2022-10-24 12:04:43 -0400

    fixed point scaling

--- src/pvp/PVPUSDC.sol
+++ src/pvp/PVPUSDC.sol
@@ -1,27 +1,66 @@
 pragma solidity ^0.8.13;
 
+import "forge-std/Test.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {BoringOwnable} from "@boringsolidity/BoringOwnable.sol";
+import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
+import {SafeCast} from "v3-core/contracts/libraries/SafeCast.sol";
 
-contract PVPUSDC is ERC20("Backed PVP USDC", "PVPUSDC", 6), BoringOwnable {
-    address staker;
-    modifier onlyOwnerOrStaker() {
-        require(
-            msg.sender == staker || msg.sender == owner,
-            "Ownable: caller is not the owner"
-        );
-        _;
+contract PVPUSDC is
+    ERC20("Backed PVP USDC", "PVPUSDC", 6),
+    BoringOwnable,
+    Test
+{
+    using SafeCast for uint256;
+
+    struct Stake {
+        uint256 amount;
+        uint256 depositedAt;
     }
+
+    mapping(address => Stake) public balance;
+
+    error AlreadyStaking();
+
+    uint256 public constant SECONDS_PER_YEAR = 365 days;
+    uint256 public APR = 1.1e18;
 
     constructor() {
         transferOwnership(msg.sender, false, false);
     }
 
-    function setStaker(address _staker) external onlyOwner {
-        staker = _staker;
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
     }
 
-    function mint(address to, uint256 amount) external onlyOwnerOrStaker {
-        _mint(to, amount);
+    function stake(uint256 amount) public {
+        if (balance[msg.sender].amount != 0) {
+            revert AlreadyStaking();
+        }
+        Stake memory stake = Stake({
+            amount: amount,
+            depositedAt: block.timestamp
+        });
+        balance[msg.sender] = stake;
+        _burn(msg.sender, amount);
+    }
+
+    function unstake() public returns (uint256) {
+        Stake memory stake = balance[msg.sender];
+        delete balance[msg.sender];
+
+        uint256 secondsElapsed = block.timestamp - stake.depositedAt;
+        uint256 ratio = FixedPointMathLib.divWadDown(
+            secondsElapsed,
+            SECONDS_PER_YEAR
+        );
+
+        uint256 total = (stake.amount *
+            uint256(
+                FixedPointMathLib.powWad(APR.toInt256(), ratio.toInt256())
+            )) / FixedPointMathLib.WAD;
+        _mint(msg.sender, total);
+
+        return total;
     }
 }
--- test/pvp/PVPUSDC.t.sol
+++ test/pvp/PVPUSDC.t.sol
@@ -2,67 +2,40 @@ pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
 import {PVPUSDC} from "src/pvp/PVPUSDC.sol";
-import {PVPStaker} from "src/pvp/PVPStaker.sol";
 
 contract PVPUSDCTest is Test {
     PVPUSDC pvpUSDC;
-    PVPStaker staker;
+
+    uint256 amount = 100;
 
     function setUp() public {
         pvpUSDC = new PVPUSDC();
-        staker = new PVPStaker(pvpUSDC);
         pvpUSDC.claimOwnership();
-        pvpUSDC.setStaker(address(staker));
     }
 
     function testMintFailsIfNotOwner() public {
         vm.startPrank(address(1));
         vm.expectRevert("Ownable: caller is not the owner");
-        pvpUSDC.mint(address(1), 100);
+        pvpUSDC.mint(address(1), amount);
         vm.stopPrank();
     }
 
     function testMintWorksIfOwner() public {
-        pvpUSDC.mint(address(1), 100);
-        assertEq(100, pvpUSDC.balanceOf(address(1)));
+        pvpUSDC.mint(address(1), amount);
+        assertEq(amount, pvpUSDC.balanceOf(address(1)));
     }
 
     function testStakingWorks() public {
-        pvpUSDC.mint(address(1), 1e6);
+        pvpUSDC.mint(address(1), amount);
         vm.startPrank(address(1));
-        pvpUSDC.approve(address(staker), 1e6);
-        staker.stake(1e6);
+        pvpUSDC.approve(address(1), type(uint256).max);
+        pvpUSDC.stake(amount);
 
         vm.warp(block.timestamp + 365 days);
-        uint256 total = staker.unstake();
-        assertEq(total, 10999905); // ~110 when scaled down, i.e. (10% of 100 + 100)
+        uint256 total = pvpUSDC.unstake();
+        assertEq(total, 109);
         assertEq(pvpUSDC.balanceOf(address(1)), total);
-        assertEq(pvpUSDC.balanceOf(address(staker)), 0);
-
-        vm.stopPrank();
-    }
-
-    function testStakeFailsIfNotLongerThanOneDay() public {
-        pvpUSDC.mint(address(1), 1e6);
-        vm.startPrank(address(1));
-        pvpUSDC.approve(address(staker), 1e6);
-        staker.stake(1e6);
-
-        vm.warp(block.timestamp + 10);
-        vm.expectRevert(PVPStaker.StakeTooShort.selector);
-        staker.unstake();
-
-        vm.stopPrank();
-    }
-
-    function testStakeFailsIfAlreadyStaking() public {
-        pvpUSDC.mint(address(1), 1e6);
-        vm.startPrank(address(1));
-        pvpUSDC.approve(address(staker), 1e6);
-        staker.stake(1e4);
-
-        vm.expectRevert(PVPStaker.AlreadyStaking.selector);
-        staker.stake(1e2);
+        assertEq(pvpUSDC.balanceOf(address(pvpUSDC)), 0);
 
         vm.stopPrank();
     }
