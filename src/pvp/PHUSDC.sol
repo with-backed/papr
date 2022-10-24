076ced55b67a8384f225d6f5529159e62dd1d16c
Adam Gobran <adamgobes@gmail.com>
2022-10-24 16:59:24 -0400

    bit more cleanup

--- src/pvp/PHUSDC.sol
+++ src/pvp/PHUSDC.sol
@@ -8,14 +8,14 @@ import {SafeCast} from "v3-core/contracts/libraries/SafeCast.sol";
 contract PHUSDC is ERC20("PAPR Heroes USDC", "phUSDC"), BoringOwnable {
     using SafeCast for uint256;
 
+    error StakingTooMuch();
+
     struct Stake {
         uint256 amount;
         uint256 depositedAt;
     }
 
     mapping(address => Stake) public stakeInfo;
-
-    error StakingTooMuch();
 
     uint256 public constant SECONDS_PER_YEAR = 365 days;
     uint256 public APR = 1.1e18;
@@ -32,11 +32,11 @@ contract PHUSDC is ERC20("PAPR Heroes USDC", "phUSDC"), BoringOwnable {
         return super.balanceOf(account) - stakeInfo[account].amount;
     }
 
-    function mint(address to, uint256 amount) external onlyOwner {
+    function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
-    function burn(uint256 amount) external {
+    function burn(uint256 amount) public {
         _burn(msg.sender, amount);
     }
 
@@ -65,17 +65,17 @@ contract PHUSDC is ERC20("PAPR Heroes USDC", "phUSDC"), BoringOwnable {
         _mint(msg.sender, total - stake.amount);
     }
 
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
-        if (amount > balanceOf(from) && from != address(0)) {
-            revert("ERC20: transfer amount exceeds balance");
-        }
-    }
-
     function stakedBalance(Stake memory stake) public view returns (uint256 newBalance) {
         uint256 secondsElapsed = block.timestamp - stake.depositedAt;
         uint256 ratio = FixedPointMathLib.divWadDown(secondsElapsed, SECONDS_PER_YEAR);
 
         newBalance =
             (stake.amount * uint256(FixedPointMathLib.powWad(APR.toInt256(), ratio.toInt256()))) / FixedPointMathLib.WAD;
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
+        if (amount > balanceOf(from) && from != address(0)) {
+            revert("ERC20: transfer amount exceeds balance");
+        }
     }
 }
