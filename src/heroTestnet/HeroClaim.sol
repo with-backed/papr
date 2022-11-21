3d215475b9b5dda50b09c34a1f6ed77d82aa421a
Wilson Cusack <cusackw@gmail.com>
2022-11-21 18:51:43 -0500

    cleanup, need final gas snapshot

--- src/heroTestnet/HeroClaim.sol
+++ src/heroTestnet/HeroClaim.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.8.13;
 
-import {MerkleProof} from 'openzeppelin-contracts/utils/cryptography/MerkleProof.sol';
+import {MerkleProof} from "openzeppelin-contracts/utils/cryptography/MerkleProof.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {ERC721} from "solmate/tokens/ERC721.sol";
 
@@ -8,7 +8,7 @@ abstract contract MintableERC721 is ERC721 {
     function mint(address to) external virtual;
 }
 
-contract HeroClaim { 
+contract HeroClaim {
     mapping(address => bool) public claimed;
     bytes32 immutable merkleRoot;
     ERC20 phUSDC;
@@ -25,7 +25,14 @@ contract HeroClaim {
         uint8 blitCount;
     }
 
-    constructor(bytes32 _root, ERC20 _phUSDC, MintableERC721 _toadz, MintableERC721 _moonBirds, MintableERC721 _dinos, MintableERC721 _blits) {
+    constructor(
+        bytes32 _root,
+        ERC20 _phUSDC,
+        MintableERC721 _toadz,
+        MintableERC721 _moonBirds,
+        MintableERC721 _dinos,
+        MintableERC721 _blits
+    ) {
         merkleRoot = _root;
         phUSDC = _phUSDC;
         toadz = _toadz;
@@ -36,7 +43,7 @@ contract HeroClaim {
 
     function prove(bytes32 leaf, bytes32[] memory siblings) public view returns (bool) {
         // In a sparse tree, empty leaves have a value of 0, so don't allow 0 as input.
-        require(leaf != 0, 'invalid leaf value');
+        require(leaf != 0, "invalid leaf value");
         bytes32 node = leaf;
         for (uint256 i = 0; i < siblings.length; ++i) {
             bytes32 sibling = siblings[i];
@@ -49,12 +56,11 @@ contract HeroClaim {
     }
 
     function claim(AccountClaim calldata claim, bytes32[] calldata merkleProof) public {
-        require(!claimed[msg.sender], 'already claimed');
+        require(!claimed[msg.sender], "claimed");
 
         bytes32 node = ~keccak256(abi.encode(msg.sender, claim));
 
-        // require(MerkleProof.verifyCalldata(merkleProof, merkleRoot, node), 'MerkleDistributor: Invalid proof.');
-        require(prove(node, merkleProof), 'MerkleDistributor: Invalid proof.');
+        require(MerkleProof.verifyCalldata(merkleProof, merkleRoot, node), "MerkleDistributor: Invalid proof.");
 
         claimed[msg.sender] = true;
 
@@ -66,9 +72,8 @@ contract HeroClaim {
     }
 
     function _mint(address to, uint256 count, MintableERC721 nft) internal {
-        for (uint i = 0; i < count; i++) {
+        for (uint256 i = 0; i < count; i++) {
             nft.mint(to);
         }
     }
-
 }
--- test/heroTestnet/HeroClaim.t.sol
+++ test/heroTestnet/HeroClaim.t.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.13;
 import "forge-std/Test.sol";
 
 import {HeroClaim, ERC721, ERC20, MintableERC721} from "src/heroTestnet/HeroClaim.sol";
+import {HeroMerkleDropHelper} from "src/heroTestnet/HeroMerkleDropHelper.sol";
 import {TestERC20} from "test/mocks/TestERC20.sol";
 
 contract MintableNFT is MintableERC721 {
@@ -25,121 +26,65 @@ contract HeroClaimTest is Test {
     MintableNFT dinos = new MintableNFT();
     TestERC20 phUSDC = new TestERC20();
 
-    function testFuzz(address[20] memory members, HeroClaim.AccountClaim[20] memory claims) public {
-        address[] memory _members = new address[](20);
-        HeroClaim.AccountClaim[] memory _claims = new HeroClaim.AccountClaim[](20);
-        for (uint256 i = 0; i < 20; i++) {
+    function testFuzz(address[5] memory members, HeroClaim.AccountClaim[5] memory claims) public {
+        // will get a type error on the fixed length
+        // arrays from the fuzzer so we need to copy
+        // into dynamic length arrays
+        address[] memory _members = new address[](5);
+        HeroClaim.AccountClaim[] memory _claims = new HeroClaim.AccountClaim[](5);
+        for (uint256 i = 0; i < members.length; i++) {
             _members[i] = members[i];
             _claims[i] = claims[i];
         }
-        (bytes32 root, bytes32[][] memory tree) = MerkleDropHelper.constructTree(_members, _claims);
+
+        (bytes32 root, bytes32[][] memory tree) = HeroMerkleDropHelper.constructTree(_members, _claims);
+
         claimContract = new HeroClaim(root, phUSDC, toadz, moonBirds, dinos, blits);
-        for (uint256 i = 0; i < 20; i++) {
-            vm.assume(members[i] != address(0));
-            vm.assume(!claimContract.claimed(members[i]));
-            phUSDC.mint(address(claimContract), claims[i].phUSDCAmount);
-            _mint(toadz, claims[i].toadzCount);
-            _mint(blits, claims[i].blitCount);
-            _mint(moonBirds, claims[i].moonBirdCount);
-            _mint(dinos, claims[i].dinoCount);
-            bytes32[] memory proof = MerkleDropHelper.createProof(i, tree);
-            vm.prank(members[i]);
-            claimContract.claim(claims[i], proof);
-            assertEq(phUSDC.balanceOf(members[i]), claims[i].phUSDCAmount);
-            assertEq(toadz.balanceOf(members[i]), claims[i].toadzCount);
-            assertEq(blits.balanceOf(members[i]), claims[i].blitCount);
-            assertEq(moonBirds.balanceOf(members[i]), claims[i].moonBirdCount);
-            assertEq(dinos.balanceOf(members[i]), claims[i].dinoCount);
+
+        for (uint256 i = 0; i < members.length; i++) {
+            address account = members[i];
+            HeroClaim.AccountClaim memory claim = claims[i];
+
+            // NFTs will revert
+            vm.assume(account != address(0));
+            // fuzzer sometimes give same address twice
+            vm.assume(!claimContract.claimed(account));
+
+            // give the claim contract the assets it needs to 
+            // fill the claim
+            _mintAllClaims(account, claim);
+
+            bytes32[] memory proof = HeroMerkleDropHelper.createProof(i, tree);
+            vm.startPrank(account);
+            claimContract.claim(claim, proof);
+
+            _checkAllClaims(account, claim);
+
+            vm.expectRevert("claimed");
+            claimContract.claim(claim, proof);
+            vm.stopPrank();
         }
     }
 
-    function _mint(MintableNFT nft, uint256 count) internal {
+    function _checkAllClaims(address account, HeroClaim.AccountClaim memory claim) internal {
+        assertEq(phUSDC.balanceOf(account), claim.phUSDCAmount);
+        assertEq(toadz.balanceOf(account), claim.toadzCount);
+        assertEq(blits.balanceOf(account), claim.blitCount);
+        assertEq(moonBirds.balanceOf(account), claim.moonBirdCount);
+        assertEq(dinos.balanceOf(account), claim.dinoCount);
+    }
+
+    function _mintAllClaims(address account, HeroClaim.AccountClaim memory claim) internal {
+        phUSDC.mint(address(claimContract), claim.phUSDCAmount);
+        _mintNFT(toadz, claim.toadzCount);
+        _mintNFT(blits, claim.blitCount);
+        _mintNFT(moonBirds, claim.moonBirdCount);
+        _mintNFT(dinos, claim.dinoCount);
+    }
+
+    function _mintNFT(MintableNFT nft, uint256 count) internal {
         for (uint256 i = 0; i < count; i++) {
             nft.mint(address(claimContract));
-        }
-    }
-
-    function prove(bytes32 leaf, bytes32[] memory siblings) public view returns (bytes32) {
-        // In a sparse tree, empty leaves have a value of 0, so don't allow 0 as input.
-        require(leaf != 0, "invalid leaf value");
-        bytes32 node = leaf;
-        for (uint256 i = 0; i < siblings.length; ++i) {
-            bytes32 sibling = siblings[i];
-            node = keccak256(
-                // Siblings are always hashed in sorted order.
-                node > sibling ? abi.encode(sibling, node) : abi.encode(node, sibling)
-            );
-        }
-        return node;
-    }
-}
-
-/// from https://github.com/dragonfly-xyz/useful-solidity-patterns/blob/main/patterns/merkle-proofs/MerkleProofs.sol#L54
-/// with modifications
-library MerkleDropHelper {
-    // Construct a sparse merkle tree from a list of members and respective claim
-    // amounts. This tree will be sparse in the sense that rather than padding
-    // tree levels to the next power of 2, missing nodes will default to a value of
-    // 0.
-    function constructTree(address[] memory members, HeroClaim.AccountClaim[] memory claims)
-        external
-        pure
-        returns (bytes32 root, bytes32[][] memory tree)
-    {
-        require(members.length != 0 && members.length == claims.length);
-        // Determine tree height.
-        uint256 height = 0;
-        {
-            uint256 n = members.length;
-            while (n != 0) {
-                n = n == 1 ? 0 : (n + 1) / 2;
-                ++height;
-            }
-        }
-        tree = new bytes32[][](height);
-        // The first layer of the tree contains the leaf nodes, which are
-        // hashes of each member and claim amount.
-        bytes32[] memory nodes = tree[0] = new bytes32[](members.length);
-        for (uint256 i = 0; i < members.length; ++i) {
-            // Leaf hashes are inverted to prevent second preimage attacks.
-            nodes[i] = ~keccak256(abi.encode(members[i], claims[i]));
-        }
-        // Build up subsequent layers until we arrive at the root hash.
-        // Each parent node is the hash of the two children below it.
-        // E.g.,
-        //              H0         <-- root (layer 2)
-        //           /     \
-        //        H1        H2
-        //      /   \      /  \
-        //    L1     L2  L3    L4  <--- leaves (layer 0)
-        for (uint256 h = 1; h < height; ++h) {
-            uint256 nHashes = (nodes.length + 1) / 2;
-            bytes32[] memory hashes = new bytes32[](nHashes);
-            for (uint256 i = 0; i < nodes.length; i += 2) {
-                bytes32 a = nodes[i];
-                // Tree is sparse. Missing nodes will have a value of 0.
-                bytes32 b = i + 1 < nodes.length ? nodes[i + 1] : bytes32(0);
-                // Siblings are always hashed in sorted order.
-                hashes[i / 2] = keccak256(a > b ? abi.encode(b, a) : abi.encode(a, b));
-            }
-            tree[h] = nodes = hashes;
-        }
-        // Note the tree root is at the bottom.
-        root = tree[height - 1][0];
-    }
-
-    // Given a merkle tree and a member index (leaf node index), generate a proof.
-    // The proof is simply the list of sibling nodes/hashes leading up to the root.
-    function createProof(uint256 memberIndex, bytes32[][] memory tree) external pure returns (bytes32[] memory proof) {
-        uint256 leafIndex = memberIndex;
-        uint256 height = tree.length;
-        proof = new bytes32[](height - 1);
-        for (uint256 h = 0; h < proof.length; ++h) {
-            uint256 siblingIndex = leafIndex % 2 == 0 ? leafIndex + 1 : leafIndex - 1;
-            if (siblingIndex < tree[h].length) {
-                proof[h] = tree[h][siblingIndex];
-            }
-            leafIndex /= 2;
         }
     }
 }
