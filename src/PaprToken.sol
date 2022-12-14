3db6ed76d152844c0bad8e06056905f3938eacf4
Wilson Cusack <cusackw@gmail.com>
2022-12-14 03:17:10 -0500

    inline CreatorGuarded, creator -> controller

--- src/PaprToken.sol
+++ src/PaprToken.sol
@@ -3,18 +3,29 @@ pragma solidity ^0.8.13;
 
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
-import {CreatorGuarded} from "src/base/CreatorGuarded.sol";
+contract PaprToken is ERC20 {
+    error ControllerOnly();
 
-contract PaprToken is ERC20, CreatorGuarded {
+    address immutable controller;
+
+    modifier onlyController() {
+        if (msg.sender != controller) {
+            revert ControllerOnly();
+        }
+        _;
+    }
+
     constructor(string memory name, string memory symbol)
         ERC20(string.concat("papr ", name), string.concat("papr", symbol), 18)
-    {}
+    {
+        controller = msg.sender;
+    }
 
-    function mint(address to, uint256 amount) external onlyCreator {
+    function mint(address to, uint256 amount) external onlyController {
         _mint(to, amount);
     }
 
-    function burn(address account, uint256 amount) external onlyCreator {
+    function burn(address account, uint256 amount) external onlyController {
         _burn(account, amount);
     }
 }
