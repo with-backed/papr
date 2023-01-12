dba5d167f1c3c9f9b892ffa084b2bac8516fc2fc
Wilson Cusack <cusackw@gmail.com>
2023-01-12 12:21:56 -0500

    lock solc

--- src/PaprController.sol
+++ src/PaprController.sol
@@ -1,5 +1,5 @@
 //SPDX-License-Identifier: BUSL-1.1
-pragma solidity ^0.8.17;
+pragma solidity =0.8.17;
 
 import {ERC20} from "solmate/tokens/ERC20.sol";
 import {ERC721, ERC721TokenReceiver} from "solmate/tokens/ERC721.sol";
