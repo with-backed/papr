96f8a9d98bbd3ecc33e3846ba22794ad4cccee75
Wilson Cusack <cusackw@gmail.com>
2023-02-01 14:03:58 -0500

    fix

--- src/PaprController.sol
+++ src/PaprController.sol
@@ -508,7 +508,10 @@ contract PaprController is
     function _checkOraclePrice(ERC721 asset, uint256 price) internal {
         CachedPrice memory cached = cachedPriceForAsset[asset];
         if (cached.price < price) {
-            if ((price - cached.price) / (block.timestamp - cached.timestamp) > MAX_PER_SECOND_PRICE_GROWTH) {
+            if (
+                ((price - cached.price) / cached.price) / (block.timestamp - cached.timestamp)
+                    > MAX_PER_SECOND_PRICE_GROWTH
+            ) {
                 revert PriceGrowthTooFast();
             }
         }
