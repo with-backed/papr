b3efde6a37186afa6e24f25d80703e92aa4b9dd5
Wilson Cusack <cusackw@gmail.com>
2023-02-08 13:30:58 -0500

    cleaned up script

--- script/DeployPaprController.s.sol
+++ script/DeployPaprController.s.sol
@@ -18,7 +18,10 @@ contract DeployPaprController is Base {
     function run() public {
         vm.startBroadcast();
 
-        ERC721[] memory collateral = new ERC721[](10);
+        ERC721[] memory collateral = new ERC721[](3);
+        collateral[0] = ERC721(0xb7D7fe7995D1E347916fAae8e16CFd6dD21a9bAE);
+        collateral[1] = ERC721(0x6EF2C9CB23F03014d18d7E4CeEAeC497dB00247C);
+        collateral[2] = ERC721(0x8232c5Fd480C2a74d2f25d3362f262fF3511CE49);
 
         controller = new PaprController(
             "trash",
@@ -30,22 +33,6 @@ contract DeployPaprController is Base {
             deployer,
             collateral
         );
-
-        IPaprController.CollateralAllowedConfig[] memory args = new IPaprController.CollateralAllowedConfig[](3);
-        args[0] = IPaprController.CollateralAllowedConfig({
-            collateral: ERC721(0xb7D7fe7995D1E347916fAae8e16CFd6dD21a9bAE),
-            allowed: true
-        });
-        args[1] = IPaprController.CollateralAllowedConfig({
-            collateral: ERC721(0x6EF2C9CB23F03014d18d7E4CeEAeC497dB00247C),
-            allowed: true
-        });
-        args[2] = IPaprController.CollateralAllowedConfig({
-            collateral: ERC721(0x8232c5Fd480C2a74d2f25d3362f262fF3511CE49),
-            allowed: true
-        });
-
-        // controller.setAllowedCollateral(args);
 
         vm.stopBroadcast();
     }
