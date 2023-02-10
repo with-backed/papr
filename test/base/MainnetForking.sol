7c7aa206687710b3cbd549d1c926845fd523d9c7
Wilson Cusack <cusackw@gmail.com>
2023-02-10 09:09:00 -0500

    updated debug

--- test/base/MainnetForking.sol
+++ test/base/MainnetForking.sol
@@ -4,5 +4,5 @@ pragma solidity ^0.8.17;
 import "forge-std/Test.sol";
 
 contract MainnetForking is Test {
-    uint256 forkId = vm.createSelectFork(vm.envString("GOERLI_RPC_URL"), 8405083);
+    uint256 forkId = vm.createSelectFork(vm.envString("MAINNET_RPC_URL"), 16594174);
 }
